c91fe76153474a5f703583306a5327277ae515b0
jwu424 <56044809+jwu424@users.noreply.github.com>
2019-10-09 09:24:41 -0500

    New shiny version

--- rshiny/server.R
+++ rshiny/server.R
@@ -8,27 +8,63 @@
 #
 
 library(shiny)
-library(ggplot2)
 
-data <- read.csv("BodyFat.csv")
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
     res <- reactive({
-        1 * input$Var1 + 2 * input$Var2 + 3 * input$Var3
+        if (input$weight_unit == "kg"){
+            weight = input$weight * 2.20462
+        }else{
+            weight = input$weight}
+        
+        if (input$abdomen_unit == "inches"){
+            abdomen = input$abdomen * 2.54
+        }else{
+            abdomen = input$abdomen}
+        
+        if (input$wrist_unit == "inches"){
+            wrist = input$wrist * 2.54
+        }else{
+            wrist = input$wrist}
+        
+        round(-23.86796500 - 0.08350773 * weight + 0.86989031 * abdomen - 1.24546008 * wrist, 2)
     })
     
 
-    
-    output$res <- renderPrint({cat(res())})
-    output$plot <- renderPlot({
-        ggplot(data, aes(x=BODYFAT)) + geom_density() + theme_bw()})
+    output$res <- renderPrint({
+        if (is.na(sum(input$weight, input$abdomen, input$wrist)) | min(input$weight, input$abdomen, input$wrist) < 0  | res() > 50 | res() <0
+            
+            | (input$weight_unit == 'kg' & input$weight < 54.5) | (input$weight_unit == 'kg' & input$weight > 122.5)
+            | (input$weight_unit == 'lbs' & input$weight < 120) | (input$weight_unit == 'lbs' & input$weight > 270)
+            
+            | (input$abdomen_unit == 'inches' & input$abdomen < 27.5) | (input$abdomen_unit == 'inches' & input$abdomen > 51)
+            | (input$abdomen_unit == 'cm' & input$abdomen < 70) | (input$abdomen_unit == 'cm' & input$abdomen > 130)
+            
+            | (input$wrist_unit == 'inches' & input$wrist < 6) | (input$wrist_unit == 'inches' & input$wrist > 8.5)
+            | (input$wrist_unit == 'cm' & input$wrist < 15) | (input$wrist_unit == 'cm' & input$wrist > 22)){
+            cat(' ')
+        }
+        else{cat(res())}
+        })
     
     output$pp <- renderImage({
         
-        if (is.na(sum(input$Var1, input$Var2, input$Var3)) | min(input$Var1, input$Var2, input$Var3) < 0 ){
-            return(list(src = "error.png", width = 1000, height = 300, alt = "Face"))}
+        if (is.na(sum(input$weight, input$abdomen, input$wrist)) | min(input$weight, input$abdomen, input$wrist) < 0 
+            
+            | (input$weight_unit == 'kg' & input$weight < 54.5) | (input$weight_unit == 'kg' & input$weight > 122.5)
+            | (input$weight_unit == 'lbs' & input$weight < 120) | (input$weight_unit == 'lbs' & input$weight > 270)
+            
+            | (input$abdomen_unit == 'inches' & input$abdomen < 27.5) | (input$abdomen_unit == 'inches' & input$abdomen > 51)
+            | (input$abdomen_unit == 'cm' & input$abdomen < 70) | (input$abdomen_unit == 'cm' & input$abdomen > 130)
+            
+            | (input$wrist_unit == 'inches' & input$wrist < 6) | (input$wrist_unit == 'inches' & input$wrist > 8.5)
+            | (input$wrist_unit == 'cm' & input$wrist < 15) | (input$wrist_unit == 'cm' & input$wrist > 22)){
+            
+            return(list(src = "error1.png", width = 500, height = 360, alt = "Face"))}
+        else if (res() > 50 | res() <0) {
+            return(list(src = "error2.png", width = 500, height = 360, alt = "Face"))}
         else if (res() == 0){
             return(list(src = "bodyfat.jpg", width = 500, height = 300, alt = "Face"))}
         else if (res() < 18.5){
--- rshiny/ui.R
+++ rshiny/ui.R
@@ -12,37 +12,43 @@ library(shiny)
 # Define UI for application that draws a histogram
 shinyUI(fixedPage(
     h1(span("Men Body Fat Calculator", style = "font-weight: 500"), 
-       style = "font-family: 'Source Sans Pro';
-     color: #8470FF; text-align: center;
-     padding: 20px"),
+       style = "font-family: 'Source Sans Pro'; text-align: center;padding: 20px"),
 
     fixedRow(
         tabsetPanel(
-            tabPanel("Input",
-                     column(3,numericInput("Var1", "Var1:", 
-                                           min = 0, max = 100000, value = 0, step = 1)),
-                     column(3,numericInput("Var2", "Var2:", 
-                                           min = 0, max = 100000, value = 0, step = 1)),
-                     column(3,numericInput("Var3", "Var3:", 
-                                           min = 0, max = 100000, value = 0, step = 1))
-            )
-        )
-    ),
-    
-    fixedRow(
-        tabsetPanel(
             tabPanel("Output",
-                     h3(span("Men Body Fat Calculator:", verbatimTextOutput("res"), style = "font-weight: 300;float:center"), 
+                     sidebarPanel(
+                         numericInput("weight", "WEIGHT:", min = 0, max = 1000, value = 158, step = 0.5),
+                         selectInput("weight_unit", "Unit for weight:", choices = c("lbs", "kg")),
+                         p("Note: Valid input should ranges from 120 to 270 lbs or 54.5 to 122.5 kg"),
+                         
+                         numericInput("abdomen", "ABDOMEN:", min = 0, max = 1000, value = 86, step = 0.5),
+                         selectInput("abdomen_unit", "Unit for ABDOMEN:", choices = c("cm", "inches")),
+                         p("Note: Valid input should ranges from 70 to 130 cm or 27.5 to 51 inches"),
+                         
+                         numericInput("wrist", "WRIST:", min = 0, max = 1000, value = 17, step = 0.1),
+                         selectInput("wrist_unit", "Unit for WRIST:", choices = c("cm", "inches")),
+                         p("Note: Valid input should ranges from 15 to 22 cm or 6 to 8.5 inches"),
+                         
+                         submitButton("Submit", icon("refresh"))
+                     ),
+                     h3(span("Your body fat percentage (%):", verbatimTextOutput("res"), style = "font-weight: 300;float:center"), 
                         style = "font-family: 'Source Sans Pro';
                         padding: 20px;float:left"),
-                     column(10, align="left",imageOutput("pp"))
-                     ),
-            tabPanel("Contact", 
+                     imageOutput("pp")
+                     
+            ),
+            tabPanel("Info&Contact",
+                     p('Pictures used in this calculator are from following:'),
+                     tags$div(a(href="https://emojiisland.com/products/loudly-crying-face-emoji-icon/", "link to Picture1")),
+                     tags$div(a(href="https://www.bariatricmexicosurgery.com/understanding-bmi-categories-levels-of-body-mass-and-obesity/", "link to Picture2")),
+                     p("-------------------------------------------"),
                      p("If you have any problems, please contact us."),
                      p("Lu Li: lli468@wisc.edu"),
                      p("Jiawei Wu: jwu424@wisc.edu"),
                      p("Nan Yan: nyan5@wisc.edu"),
                      p("Youhui Ye: yye65@wisc.edu"))
+            
         )
     )
     
